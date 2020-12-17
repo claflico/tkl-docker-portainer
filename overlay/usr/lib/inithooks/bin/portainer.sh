8557460531bf6d2b7f18d907730489525a0040ad
Cory Claflin <github@claflinclan.com>
2020-12-17 06:40:31 +0000

    fix tar.gz name and add echos

--- overlay/usr/lib/inithooks/bin/portainer.sh
+++ overlay/usr/lib/inithooks/bin/portainer.sh
@@ -2,7 +2,7 @@
 # Loads portainer image and starts portainer
 
 IMAGE="portainer/portainer-ce"
-IMGTAR="portainer.tar.gz"
+IMGTAR="portainer.tar"
 VOL="portainer_data"
 HUB_TEST=$(curl -k -sS -m 5 -L https://hub.docker.com/v2/repositories/${IMAGE}/tags/latest | grep digest | wc -l)
 IMG_TEST=$(docker images | grep portainer/portainer-ce | wc -l)
@@ -15,19 +15,21 @@ function load_image() {
     docker pull $IMAGE:latest
   else
     echo "Loading included Portainer image"
-    gunzip /$IMGTAR
+    gunzip /$IMGTAR.gz
     docker load -i /$IMGTAR
   fi
 }
 
 function create_volume() {
   if [[ $VOL_TEST -eq 0 ]]; then
+    echo "Creating docker volume for portainer data"
     docker volume create portainer_data
   fi
 }
 
 function docker_run() {
-   docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $VOL:/data $IMAGE
+  echo "Starting Portainer container"
+  docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $VOL:/data $IMAGE
 }
 
 if [[ $IMG_TEST -eq 0 ]]; then
